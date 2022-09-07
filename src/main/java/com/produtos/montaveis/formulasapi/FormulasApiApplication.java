dcfc6bcaf9f54b43bd8314ee405b9c82e23b90e2
Gustavo Almeida <gualmeida2004@gmail.com>
2022-09-07 13:30:12 -0300

    Initial commit

---
+++ src/main/java/com/produtos/montaveis/formulasapi/FormulasApiApplication.java
@@ -0,0 +1,13 @@
+package com.produtos.montaveis.formulasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/produtos/montaveis/formulasapi/FormulasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.produtos.montaveis.formulasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
